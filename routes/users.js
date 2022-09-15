2e792d2cbb81efa8093c9e04882f07477a4b635b
nontanut <bo.nontanut@gmail.com>
2022-09-15 23:01:41 +0700

    add describes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 const {create, getAll, getGender, getStyle, getSize} = require("../controllers/usersCtrl");
 
+// All route must be /api before
 router.post("/user", create);
+// Get all data with page and per_page
 router.get("/user/getdata", getAll);
+// Get data by gender men or women
 router.get("/user/getgender/:gender", getGender);
+// Get data by style ex. blue polka dot or spiderman
 router.get("/user/getstyle/:style", getStyle);
+// Get data by size x, xs, m, l or xl
 router.get("/user/getsize/:size", getSize);
 
 module.exports = router;
